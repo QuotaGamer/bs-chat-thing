495c3e4169994fd47ec7d89172849718c14dc7df
Quota Z <quotableacorn@gmail.com>
2024-11-12 21:41:07 -0500

    added some typing

--- server/json_helper.py
+++ server/json_helper.py
@@ -1,15 +1,16 @@
 import json, threading
-from typing import Any
-print("rename this quota, should be make_json.py now")
+from typing import Any, Union
 
 class JSONManager:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str) -> None:
         self._file_path = file_path
         self._lock = threading.Lock()
         self._load_json()
 
-    def _load_json(self):
-        """Loads the JSON data from the file."""
+    def _load_json(self) -> None:
+        """Internal function.
+        
+        Loads the JSON data from the file."""
         try:
             with open(self._file_path, 'r') as f:
                 self._data = json.load(f)
@@ -18,21 +19,23 @@ class JSONManager:
         except json.JSONDecodeError:
             self._data = {}
 
-    def _save_json(self):
-        """Saves the JSON data back to the file."""
+    def _save_json(self) -> None:
+        """Internal function.
+        
+        Saves the JSON data back to the file."""
         with open(self._file_path, 'w') as f:
             json.dump(self._data, f, indent=4)
 
-    def get_data(self):
+    def get_data(self) -> Any:
         """Returns the JSON data."""
         return self._data
 
-    def update_data(self, key, value):
+    def update_data(self, key:str, value:Union[list, str, dict, int, float, None]) -> None:
         """Updates a value in the JSON data and saves it."""
         self._data[key] = value
         self._save_json()
 
-    def delete_key(self, key):
+    def delete_key(self, key) -> None:
         """Deletes a key from the JSON data and saves it."""
         if key in self._data:
             del self._data[key]
